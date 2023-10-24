4fe66f441b771355d9024544683a31d86e5ab2a0
HipsterBrown <headhipster@hipsterbrown.com>
2023-10-24 15:58:50 -0400

    fix: refactor to onboard process

--- src/main.py
+++ src/main.py
@@ -3,7 +3,6 @@ import os
 import sys
 from dotenv import dotenv_values
 
-# from viam.module.module import Module
 from audioout import Audioout
 from rgb import Rgb
 
@@ -12,32 +11,20 @@ from viam.rpc.dial import Credentials, DialOptions
 from viam.components.board import Board
 from viam.components.camera import Camera
 
-# from .code import CodeService, candy_bucket
 from viam.logging import getLogger
 
 LOGGER = getLogger(__name__)
 SPOOKY_SOUND = os.path.abspath("./code/ghostly_whisper.mp3")
 
 
-async def main(address: str):
-    """This function creates and starts a new module, after adding all desired resources.
-    Resources must be pre-registered. For an example, see the `__init__.py` file.
-    Args:
-        address (str): The address to serve the module on
-    """
-    LOGGER.info("Starting candy bucket module")
-    LOGGER.info(address)
-    # module = Module(address)
-    # module.add_model_from_registry(CodeService.SUBTYPE, candy_bucket.MODEL)
-    # await module.start()
+async def main():
     candy_bucket = CandyBucket()
     try:
         await candy_bucket.start()
     except Exception as error:
         LOGGER.error("Error starting up candy bucket", exc_info=error)
     finally:
-        LOGGER.info("Stopping candy bucket program")
-        await candy_bucket.stop()
+        LOGGER.info("Stopped candy bucket program")
 
 
 class CandyBucket:
@@ -47,11 +34,10 @@ class CandyBucket:
     speaker: Audioout
     lights: Rgb
     last_tick: int = 0
-    started: asyncio.Task
     q: asyncio.Queue
 
     def __init__(self):
-        self.config = dotenv_values(os.path.abspath("../.env"))
+        self.config = dotenv_values(os.path.abspath(".env"))
 
     async def start(self):
         robot_secret = self.config.get("ROBOT_SECRET")
@@ -80,25 +66,11 @@ class CandyBucket:
         self.speaker = Audioout.from_robot(robot, name=speaker_name)
         self.lights = Rgb.from_robot(robot, lights_name)
 
-        # board = dependencies[Board.get_resource_name(board_name)]
-        # camera = dependencies[Camera.get_resource_name(camera_name)]
-        # speaker = dependencies[Audioout.get_resource_name(speaker_name)]
-        # lights = dependencies[Rgb.get_resource_name(lights_name)]
-
-        # self.board = cast(Board, board)
-        # self.camera = cast(Camera, camera)
-        # self.motion_sensor_name = motion_sensor_name
-        # self.speaker = cast(Audioout, speaker)
-        # self.lights = cast(Rgb, lights)
-
-        LOGGER.info("<-----STARTED CANDY BUCKET----->")
-
-        if self.started:
-            self.started.cancel()
-
-        self.started = asyncio.create_task(self.run())
-
-        return self
+        LOGGER.info("<-----STARTING CANDY BUCKET----->")
+        try:
+            await self.run()
+        finally:
+            await robot.close()
 
     async def connect(self):
         creds = Credentials(
@@ -134,12 +106,6 @@ class CandyBucket:
 
         LOGGER.info(results)
 
-    async def stop(self):
-        LOGGER.info("<-----CLOSING CANDY BUCKET----->")
-        if self.started:
-            self.started.cancel()
-            await self.started
-
     async def poll_motion(self) -> None:
         while True:
             next_tick = await self.motion_sensor.value()
@@ -169,7 +135,4 @@ class CandyBucket:
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        raise Exception("Need socket path as command line argument")
-
-    asyncio.run(main(sys.argv[1]))
+    asyncio.run(main())
